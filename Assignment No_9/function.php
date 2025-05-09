1f947d69180edde8b2c31175bfd51a7612933191
Kaif38 <darwajkarkaif@gmail.com>
2025-05-09 10:42:07 +0530

    Add files via upload

---
+++ Assignment No_9/function.php
@@ -0,0 +1,20 @@
+<?php
+function greet($name) {
+    return "Hello, $name!";
+}
+echo greet("kaif");
+?>
+
+<?php
+function addNumbers($a, $b) {
+    return $a + $b;
+}
+echo addNumbers(5, 10);
+?>
+
+<?php
+function multiply($a, $b) {
+    return $a * $b;
+}
+echo multiply(5, 10);
+?>
